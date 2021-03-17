c33b6492a8be92b834a0506da7c5b726da3887fa
Keivan Zavari <keivan.zavari@gmail.com>
2021-03-17 20:24:14 +0100

    Add transitions

---
+++ src/transitions.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "types.h"
+
+namespace fsm {
+
+class Transition {
+ public:
+  // Transition(const std::string& from_, const std::string& to_, const EventType& event)
+  //     : from(from_), to(to_), events({event}) {
+  //   updateTable();
+  // }
+  Transition(const std::string& from_, const std::string& to_, const std::vector<EventType>& events_)
+      : from(from_), to(to_), events(events_) {
+    updateTable();
+  }
+  // this means that if state "from" is done, and the done event is fired, we should go to state "to",
+  // therefore there should be a mapping from this event to the target state "to".
+  // that state should run and that state.enter method should be executed.
+  // * This mean that the state machine should own an instance of that class and execute it.
+  // example:
+  // from init to A with e_init
+  // from A to B with e_done, with e_random
+  // from A to C with e_quick
+  // from B to C with e_done
+  // from C to A with e_restart
+  void updateTable() {
+    for (auto& event : events) {
+      table[from][to].emplace(event);
+    }
+  }
+
+  static TransitionTable& getTable() { return Transition::table; }
+
+ private:
+  const std::string from;
+  const std::string to;
+  const std::vector<EventType> events;
+
+  static TransitionTable table;
+};
+
+TransitionTable Transition::table{};
+
+}  // namespace fsm
--- src/types.h
+++ src/types.h
@@ -1,7 +1,16 @@
 #pragma once
+
+#include <set>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace fsm {
 using EventType = std::string;
 using MsgType = std::string;
+
+using EventsType = std::set<EventType>;
+
+using TransitionTable = std::unordered_map<std::string, std::unordered_map<std::string, EventsType>>;
+
 }  // namespace fsm
